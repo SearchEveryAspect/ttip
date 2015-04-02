bbda71a77ccba5a2694846fe84b9dcd8580d5f4d
aguler <alan.guler@hotmail.com>
2015-04-02 22:10:43 +0200

    Made navigation bar sticky on scroll (works on Chrome)

--- frontend/scripts/sticky.js
+++ frontend/scripts/sticky.js
@@ -1,21 +1,21 @@
 //@author: Alan Güler
 //@date: 2015-04-01
 
-// @author: Alan Güler
-// @date: 2015-04-01 
-
-//TODO make navigation bar sticky on scroll
-
+//Make navigation bar sticky on scroll
 
 jQuery(document).ready(function() {
 	var navOff = jQuery("#header").offset().top;
-	var scrollPos = jQuery(window).scrollTop();
-
-	if (scrollPos >= navOff) {
-
-	}else {
-
-	}
 	
+	jQuery("#header-wrapper").height(jQuery("nav").outerHeight("#header"));
+	jQuery(window).scroll(function() {
+
+		var scrollPos = jQuery(window).scrollTop();
+
+		if (scrollPos >= navOff) {
+			jQuery("#header").addClass("sticky");
+		}else {
+			jQuery("#header").removeClass("sticky");
+		}
+	});
 
 });
