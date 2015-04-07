741b386f6059750ac0c7b0b658c796ef43f5cd83
Jacqueline Eriksson <jaceri@kth.se>
2015-04-07 22:33:45 +0200

    implemented ESDocument object

---
+++ backend/src/com/searcheveryaspect/backend/ESDocument.java
@@ -0,0 +1,30 @@
+package com.searcheveryaspect.backend;
+
+/**
+ * 
+ * Specifies the ESDocument object
+ * @author Jacqueline Eriksson
+ *
+ */
+
+public class ESDocument {
+	String docId;
+	String publishedTimestamp;
+	String fetchedTimestamp;
+	String title;
+	String[] category;
+	String party;
+	
+	public ESDocument(String docId, String publishedTimestamp, String fetchedTimestamp, String title, String[] category, String party) {
+	this.docId = docId;
+	this.publishedTimestamp = publishedTimestamp;
+	this.fetchedTimestamp = fetchedTimestamp;
+	this.title = title;
+	this.category = category;
+	this.party = party;		
+	}
+	
+	
+	
+
+}
