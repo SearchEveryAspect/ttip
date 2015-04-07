9a6744172c2459d7f13585cda18f51d04ef2812e
Jacqueline Eriksson <jaceri@kth.se>
2015-04-07 22:32:12 +0200

    added createESDocument method

--- backend/src/com/searcheveryaspect/backend/ESDocumentBuilder.java
+++ backend/src/com/searcheveryaspect/backend/ESDocumentBuilder.java
@@ -1,8 +1,12 @@
 package com.searcheveryaspect.backend;
 
+import org.joda.time.LocalDate;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
 /**
  * 
- * Builds the documents that shall be stored in Elasticsearch. 
+ * Create an ESDocument	
  * @author Jacqueline Eriksson
  *
  */
@@ -10,6 +14,36 @@ package com.searcheveryaspect.backend;
 public class ESDocumentBuilder {
 	
 	
+	// tar ett GovDocumentLite 
+	public ESDocument createESDocument(GovDocumentLite doc){
+				
+		//måste se till att date i doc id har rätt format	
+		String docId = doc.getId();
+		
+		String publishedTimestamp = doc.getDate(); //fast vill ju inte ha datumet i stäng format 
+			
+		LocalDate localDate = new LocalDate();		 
+		DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy-MM-dd");
+		String fetchedTimestamp = formatter.print(localDate);
+		
+		String title = doc.getTitle();
+		
+		String[] category = new String[]{"Unknown"};
+		
+		//categhory-> hämtas ur NLP // Vill man hitta caqtegorier innaan man skapar JSON, skapas JSON doc i ES documentbuilder för att 
+		//sedan skickas in i databsen i ElasticSearchPut? vad används i så fall ESDocument till? Sätt till unknown så länge 
+		
+		//party ->hur får jag ut partiet? tittar jag på vilka personer som skrivit motionen för att därefter gå in och titta på 
+		//vilket parti dessa personer är ifrån. 
+		
+		String party = "Unknown";
+		
+		ESDocument eSDoc = new ESDocument(docId, publishedTimestamp, fetchedTimestamp, title, category, party);
+		
+		return eSDoc;
+		
+	}
 
 }
 	
+
