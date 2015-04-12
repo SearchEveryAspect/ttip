c2f71b93a2c5813fb57048d098bd28009a22c80b
Mitra Strandberg <mitrastrandberg@gmail.com>
2015-04-12 18:25:42 +0200

    Added response class

---
+++ backend/src/com/searcheveryaspect/backend/Response.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.searcheveryaspect.backend;
+
+/**
+ * Gets the response from ES.
+ * @author Mitra
+ *
+ */
+public class Response {
+	String category;
+	String[] labels;
+	Dataset[] datasets;
+	
+	public Response() {
+		
+	}
+
+}
