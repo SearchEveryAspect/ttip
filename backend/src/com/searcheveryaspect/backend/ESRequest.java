4a0ec609c49c14d0c85a04955413690c46bb1a1a
Mitra Strandberg <mitrastrandberg@gmail.com>
2015-04-14 21:29:43 +0200

    Fixed some comments

--- backend/src/com/searcheveryaspect/backend/ESRequest.java
+++ backend/src/com/searcheveryaspect/backend/ESRequest.java
@@ -4,6 +4,10 @@
 package com.searcheveryaspect.backend;
 
 /**
+ * Contains information about what the ESQuerier will search for.
+ * Get search info from frontend.
+ * Creates a querier depending on information in fields and querier returns the result.
+ * Then creates a Response depending on the search result.
  * @author Mitra
  *
  */
