0a9b32aa0887f93d31171323382439d545e0afe4
aguler <alan.guler@hotmail.com>
2015-04-16 16:07:31 +0200

    modified sticky and added dropdown menu
    Former-commit-id: aac4597341006a8357eee330d735d66329435ea6

---
+++ frontend/scripts/navBar.js
@@ -0,0 +1,23 @@
+//@author: aguler
+
+/*Document scroll on nav-dropdown click*/
+
+$(document).ready(function() {
+	$("#0").click(function() {
+	    $('html, body').animate({
+	        scrollTop: $(".chartcont0").offset().top- 42
+	    }, 800);
+	});
+
+	$("#1").click(function() {
+	    $('html, body').animate({
+	        scrollTop: $(".chartcont1").offset().top- 42
+	    }, 800);
+	});
+
+	$("#2").click(function() {
+	    $('html, body').animate({
+	        scrollTop: $(".chartcont2").offset().top - 41
+	    }, 800);
+	});
+});
