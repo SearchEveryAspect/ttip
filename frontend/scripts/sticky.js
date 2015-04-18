6c3fb5c3dcfa9ba9deab8842cff77a8187cf8bfe
aguler <alan.guler@hotmail.com>
2015-04-18 02:27:05 +0200

    New design (adapted only for screen resolutions 1280x800 for now)
    Former-commit-id: 0adc48dc6662e65a337a48b18917e898fb8b347b

--- frontend/scripts/sticky.js
+++ frontend/scripts/sticky.js
@@ -3,25 +3,19 @@
 //Make navigation bar sticky on scroll
 
 $(document).ready(function() {
-	var navOff = $("#header").offset().top;
-	var navStyleOrig = {"height": "35px"};
-	var navStyleScroll = {"margin-left": "100000px", "height": "35px"};
-	$("#header-wrapper").height(jQuery("#header").outerHeight());
+	var navOff = $("nav").offset().top;
+	$("#header-wrapper").height(jQuery("nav").outerHeight());
 
 	$(window).scroll(function() {
 
 		var scrollPos = jQuery(window).scrollTop();
 
 		if (scrollPos >= navOff) {
+			$("nav").addClass("sticky");
 
-			//TODO:add title next to nav-bar
-			$("#header").addClass("sticky");
-			$("nav").css(navStyleScroll);
 		}else {
-			$("#header").removeClass("sticky");
-			$("#header").addClass("headline");
+			$("nav").removeClass("sticky");
 
-			$("nav").css(navStyleOrig);
 
 		}
 	});
