fdd4b83f049e64a4ff848f65c85c2cd629803291
Jenny Norelius <jenny.norelius@gmail.com>
2015-04-22 22:32:02 +0200

    merge resulted in error

--- backend/src/com/searcheveryaspect/backend/ESDocument.java
+++ backend/src/com/searcheveryaspect/backend/ESDocument.java
@@ -3,38 +3,34 @@ package com.searcheveryaspect.backend;
 /**
  * 
  * Specifies the ESDocument object
+ * 
  * @author Jacqueline Eriksson
- *
+ * 
  */
 
 public class ESDocument {
-	String docId;
-	long publishedTimestamp; //in unix
-	long fetchedTimestamp; //in  unix
-	String title;
-	String[] category;
-	String party;
-	
-	
-	public ESDocument(String docId, long publishedTimestamp, long fetchedTimestamp, String title, String[] category, String party) {
-	this.docId = docId;
-	this.publishedTimestamp = publishedTimestamp;
-	this.fetchedTimestamp = fetchedTimestamp;
-	this.title = title;
-	this.category = category;
-	this.party = party;		
-	}
+  String docId;
+  long publishedTimestamp; // in unix
+  long fetchedTimestamp; // in unix
+  String title;
+  String[] category;
+  String party;
 
-  /**
-   * @return
-   */
-  public String getDocId() {
-    return null;
+
+  public ESDocument(String docId, long publishedTimestamp, long fetchedTimestamp, String title,
+      String[] category, String party) {
+    this.docId = docId;
+    this.publishedTimestamp = publishedTimestamp;
+    this.fetchedTimestamp = fetchedTimestamp;
+    this.title = title;
+    this.category = category;
+    this.party = party;
   }
-	
-	public String getDocId() {
-		return docId;
-	}
-	
+
+
+  public String getDocId() {
+    return docId;
+  }
+
 
 }
