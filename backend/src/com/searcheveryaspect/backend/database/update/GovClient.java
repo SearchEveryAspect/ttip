c245e089cd0c6d2c060f5d2fdb0b4915a3fe09ff
Jenny Norelius <jenny.norelius@gmail.com>
2015-04-27 20:11:40 +0200

    Made GovFetchRequest fields immutable and added a builder

--- backend/src/com/searcheveryaspect/backend/database/update/GovClient.java
+++ backend/src/com/searcheveryaspect/backend/database/update/GovClient.java
@@ -1,85 +1,84 @@
 package com.searcheveryaspect.backend.database.update;
 
-import java.io.BufferedReader;
+import com.google.gson.Gson;
+
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Calendar;
 
-import com.google.gson.Gson;
 
 
+public class GovClient {
 
-public class GovClient 
-{
-	
-	public static final int TRAFFAR_LIMIT = 5000;
-	
-	public ArrayList<GovDocumentList> fetchDocs(GovFetchRequest request) throws Exception
-	{
-		GovDocumentList fetched;
-		String json = null;
-		Gson gson = new Gson();
-		ArrayList<GovDocumentList> result;
-		
-		//Gets the initial request
-		try {
-			json = URLConnectionReader.getText(request.toString());
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			return null;
-		}
+  public static final int TRAFFAR_LIMIT = 5000;
 
-		gson = new Gson();
-		result = new ArrayList<GovDocumentList>();
-		fetched = gson.fromJson(json, GovSearchResult.class).dokumentlista;
-		
-		
-		result.add(fetched);
-		
-		if(fetched.warning != null && fetched.traffar > TRAFFAR_LIMIT)
-		{
-			System.out.println(fetched.warning);
-			
-			//Create two new requests with the same period divided into two
-			GovDate mean = request.period.returnMean();
-			System.out.println("From: " + request.period.getFrom());
-			System.out.println("To: " + request.period.getTo());
-			System.out.println("Mean : " + mean);
-			Period period1 = new Period(request.period.from, mean);
-			GovFetchRequest req1 = new GovFetchRequest(request.searchString, request.rm , period1 , request.ts, request.bet,
-					request.tempbet, request.nr, request.organ, request.commissionerId, request.parties);
-			
-			Period period2 = new Period(new GovDate(mean.year, mean.month, mean.day + 1), request.period.to);
-			GovFetchRequest req2 = new GovFetchRequest(request.searchString, request.rm , period2 , request.ts, request.bet,
-					request.tempbet, request.nr, request.organ, request.commissionerId, request.parties);
-			
-			result.addAll(fetchDocs(req1));
-			result.addAll(fetchDocs(req2));
-		}
-		else {
-			int i = 1;
-			//Checks if there are more pages found
-			while(fetched.existsNextPage())
-			{
-				System.out.println("Fetching page " + i + " out of " + fetched.sidor);
-				json = URLConnectionReader.getText(fetched.nextPage());
-				System.out.println("Parsing page " + i + " out of " + fetched.sidor);
-				fetched = gson.fromJson(json, GovSearchResult.class).dokumentlista;
-				//Adds extra pages
-				result.add(fetched);
-				i++;
-			}
-		}
-		
-		return result;
-		
-	}
-	
-	public ArrayList<GovDocumentList> fetchAllDocs() throws Exception
-	{
-		Calendar d = Calendar.getInstance();
-		return fetchDocs(new GovFetchRequest("", "", new Period(new GovDate(1900, 1, 1),new GovDate(d.get(Calendar.YEAR), d.get(Calendar.MONTH), d.get(Calendar.DAY_OF_MONTH))), "", "", "", "", "", new ArrayList<String>()));
-	}
+  public ArrayList<GovDocumentList> fetchDocs(GovFetchRequest request) throws Exception {
+    GovDocumentList fetched;
+    String json = null;
+    Gson gson = new Gson();
+    ArrayList<GovDocumentList> result;
+
+    // Gets the initial request
+    try {
+      json = URLConnectionReader.getText(request.toString());
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+      return null;
+    }
+
+    gson = new Gson();
+    result = new ArrayList<GovDocumentList>();
+    fetched = gson.fromJson(json, GovSearchResult.class).dokumentlista;
+
+
+    result.add(fetched);
+
+    if (fetched.warning != null && fetched.traffar > TRAFFAR_LIMIT) {
+      System.out.println(fetched.warning);
+
+      // Create two new requests with the same period divided into two
+      GovDate mean = request.getPeriod().returnMean();
+      System.out.println("From: " + request.getPeriod().getFrom());
+      System.out.println("To: " + request.getPeriod().getTo());
+      System.out.println("Mean : " + mean);
+      Period period1 = new Period(request.getPeriod().from, mean);
+      GovFetchRequest req1 =
+          new GovFetchRequest(request.getSearchString(), request.getRm(), period1, request.getTs(),
+              request.getBet(), request.getTempbet(), request.getNr(), request.getOrgan(),
+              request.getCommissionerId(), request.getParties());
+
+      Period period2 =
+          new Period(new GovDate(mean.year, mean.month, mean.day + 1), request.getPeriod().to);
+      GovFetchRequest req2 =
+          new GovFetchRequest(request.getSearchString(), request.getRm(), period2, request.getTs(),
+              request.getBet(), request.getTempbet(), request.getNr(), request.getOrgan(),
+              request.getCommissionerId(), request.getParties());
+
+      result.addAll(fetchDocs(req1));
+      result.addAll(fetchDocs(req2));
+    } else {
+      int i = 1;
+      // Checks if there are more pages found
+      while (fetched.existsNextPage()) {
+        System.out.println("Fetching page " + i + " out of " + fetched.sidor);
+        json = URLConnectionReader.getText(fetched.nextPage());
+        System.out.println("Parsing page " + i + " out of " + fetched.sidor);
+        fetched = gson.fromJson(json, GovSearchResult.class).dokumentlista;
+        // Adds extra pages
+        result.add(fetched);
+        i++;
+      }
+    }
+
+    return result;
+
+  }
+
+  public ArrayList<GovDocumentList> fetchAllDocs() throws Exception {
+    Calendar d = Calendar.getInstance();
+    return fetchDocs(new GovFetchRequest("", "", new Period(new GovDate(1900, 1, 1), new GovDate(
+        d.get(Calendar.YEAR), d.get(Calendar.MONTH), d.get(Calendar.DAY_OF_MONTH))), "", "", "",
+        "", "", new ArrayList<String>()));
+  }
 }
--- backend/src/com/searcheveryaspect/backend/database/update/GovFetchRequest.java
+++ backend/src/com/searcheveryaspect/backend/database/update/GovFetchRequest.java
@@ -1,95 +1,231 @@
 package com.searcheveryaspect.backend.database.update;
 
 
-import java.util.ArrayList;
+import com.google.common.collect.ImmutableList;
 
+import java.util.List;
 
-public class GovFetchRequest
-{
+/**
+ * Represents a fetch request to Oppna Data API.
+ * 
+ */
+public class GovFetchRequest {
 
-	private final String RIKSDAG_URL = "http://data.riksdagen.se/dokumentlista/";
-	//Sökord
-	String searchString;
-	//Riksmöte
-	String rm;
+  private final String RIKSDAG_URL = "http://data.riksdagen.se/dokumentlista/";
+  // Sökord
+  private final String searchString;
+  // Riksmöte
+  private final String rm;
 
-	Period period;
-	//Systemdatum
-	String ts;
-	//Beteckning
-	String bet;
-	//Tempbet
-	String tempbet;
-	//Nummer
-	String nr;
-	//Utskott/Organ
-	String organ;
-	//Ledamot
-	int commissionerId; //-1 if not used
-	//List of different parties
-	ArrayList<String> parties;
+  private final Period period;
+  // Systemdatum
+  private final String ts;
+  // Beteckning
+  private final String bet;
+  // Tempbet
+  private final String tempbet;
+  // Nummer
+  private final String nr;
+  // Utskott/Organ
+  private final String organ;
+  // Ledamot
+  private final int commissionerId; // -1 if not used
+  // List of different parties
+  private final ImmutableList<String> parties;
 
-	public GovFetchRequest(String searchString, String rm, Period period, String ts, String bet,
-			String tempbet, String nr, String organ, int commissionerId, ArrayList<String> parties)
-	{
-		this.searchString = searchString;
-		this.rm = rm;
-		this.period = period;
-		this.ts = ts;
-		this.bet = bet;
-		this.tempbet = tempbet;
-		this.nr = nr;
-		this.organ = organ;
-		this.commissionerId = commissionerId;
-		this.parties = parties;
-	}
+  public GovFetchRequest(String searchString, String rm, Period period, String ts, String bet,
+      String tempbet, String nr, String organ, int commissionerId, List<String> parties) {
+    this.searchString = searchString;
+    this.rm = rm;
+    this.period = period;
+    this.ts = ts;
+    this.bet = bet;
+    this.tempbet = tempbet;
+    this.nr = nr;
+    this.organ = organ;
+    this.commissionerId = commissionerId;
+    this.parties = ImmutableList.copyOf(parties);
+  }
 
-	public GovFetchRequest(String searchString, String rm, Period period, String ts, String bet,
-			String tempbet, String nr, String organ, ArrayList<String> parties)
-	{
-		this.searchString = searchString;
-		this.rm = rm;
-		this.period = period;
-		this.ts = ts;
-		this.bet = bet;
-		this.tempbet = tempbet;
-		this.nr = nr;
-		this.organ = organ;
-		this.commissionerId = -1;
-		this.parties = parties;
-	}
+  public GovFetchRequest(String searchString, String rm, Period period, String ts, String bet,
+      String tempbet, String nr, String organ, List<String> parties) {
+    this.searchString = searchString;
+    this.rm = rm;
+    this.period = period;
+    this.ts = ts;
+    this.bet = bet;
+    this.tempbet = tempbet;
+    this.nr = nr;
+    this.organ = organ;
+    this.commissionerId = -1;
+    this.parties = ImmutableList.copyOf(parties);
+  }
 
-	public String toString()
-	{
-		StringBuilder sb = new StringBuilder();
-		sb.append(RIKSDAG_URL);
-		sb.append("?sok=");
-		sb.append(searchString.replace(" ", "%20"));
-		sb.append("&doktyp=mot&rm=");
-		sb.append(rm);
-		sb.append("&from=");
-		sb.append(period.getFrom());
-		sb.append("&tom=");
-		sb.append(period.getTo());
-		sb.append("&ts=");
-		sb.append(ts);
-		sb.append("&bet=");
-		sb.append(bet);
-		sb.append("&tempbet=");
-		sb.append(tempbet);
-		sb.append("&nr=");
-		sb.append("");
-		sb.append("&org=");
-		sb.append(organ);
-		sb.append("&iid=");
-		if(commissionerId != -1)
-			sb.append(commissionerId);
-		for(int i = 0; i < parties.size(); i++)
-		{
-			sb.append("&parti=");
-			sb.append(parties.get(i));
-		}
-		sb.append("&webbtv=&talare=&exakt=&planering=&sort=rel&sortorder=desc&rapport=&utformat=json&a=s#soktraff");
-		return sb.toString();
-	}
+  private GovFetchRequest(Builder b) {
+    this.searchString = b.searchString;
+    this.rm = b.rm;
+    this.period = b.period;
+    this.ts = b.ts;
+    this.bet = b.bet;
+    this.tempbet = b.tempbet;
+    this.nr = b.nr;
+    this.organ = b.organ;
+    this.commissionerId = b.commissionerId;
+    this.parties = ImmutableList.copyOf(b.parties);
+  }
+
+  public String getRIKSDAG_URL() {
+    return RIKSDAG_URL;
+  }
+
+  public String getSearchString() {
+    return searchString;
+  }
+
+  public String getRm() {
+    return rm;
+  }
+
+  public Period getPeriod() {
+    return period;
+  }
+
+  public String getTs() {
+    return ts;
+  }
+
+  public String getBet() {
+    return bet;
+  }
+
+  public String getTempbet() {
+    return tempbet;
+  }
+
+  public String getNr() {
+    return nr;
+  }
+
+  public String getOrgan() {
+    return organ;
+  }
+
+  public int getCommissionerId() {
+    return commissionerId;
+  }
+
+  public ImmutableList<String> getParties() {
+    return parties;
+  }
+
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append(RIKSDAG_URL);
+    sb.append("?sok=");
+    sb.append(searchString.replace(" ", "%20"));
+    sb.append("&doktyp=mot&rm=");
+    sb.append(rm);
+    sb.append("&from=");
+    sb.append(period.getFrom());
+    sb.append("&tom=");
+    sb.append(period.getTo());
+    sb.append("&ts=");
+    sb.append(ts);
+    sb.append("&bet=");
+    sb.append(bet);
+    sb.append("&tempbet=");
+    sb.append(tempbet);
+    sb.append("&nr=");
+    sb.append("");
+    sb.append("&org=");
+    sb.append(organ);
+    sb.append("&iid=");
+    if (commissionerId != -1) sb.append(commissionerId);
+    for (int i = 0; i < parties.size(); i++) {
+      sb.append("&parti=");
+      sb.append(parties.get(i));
+    }
+    sb.append("&webbtv=&talare=&exakt=&planering=&sort=rel&sortorder=desc&rapport=&utformat=json&a=s#soktraff");
+    return sb.toString();
+  }
+
+  private static class Builder {
+    private String searchString;
+    // Riksmöte
+    private String rm;
+
+    private Period period;
+    // Systemdatum
+    private String ts;
+    // Beteckning
+    private String bet;
+    // Tempbet
+    private String tempbet;
+    // Nummer
+    private String nr;
+    // Utskott/Organ
+    private String organ;
+    // Ledamot
+    private int commissionerId; // -1 if not used
+    // List of different parties
+    private List<String> parties;
+
+    public Builder searchString(String s) {
+      searchString = s;
+      return this;
+    }
+
+    public Builder riksmote(String s) {
+      rm = s;
+      return this;
+    }
+
+    public Builder period(Period p) {
+      period = p;
+      return this;
+    }
+
+    public Builder systemDate(String s) {
+      ts = s;
+      return this;
+    }
+
+    public Builder beteckning(String s) {
+      bet = s;
+      return this;
+    }
+
+    public Builder tempBeteckning(String s) {
+      tempbet = s;
+      return this;
+    }
+
+    public Builder number(String s) {
+      nr = s;
+      return this;
+    }
+
+    public Builder organ(String s) {
+      organ = s;
+      return this;
+    }
+
+    public Builder tempBeteckning(int i) {
+      commissionerId = i;
+      return this;
+    }
+
+    public Builder parties(List<String> l) {
+      parties = l;
+      return this;
+    }
+
+    public GovFetchRequest build() {
+      return new GovFetchRequest(this);
+    }
+  }
+
+  public static Builder newGovFetchRequest() {
+    return new Builder();
+  }
 }
