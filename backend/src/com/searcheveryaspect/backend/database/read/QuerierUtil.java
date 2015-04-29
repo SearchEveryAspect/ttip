910d68cc065f84b99f15f6b1d645cb50e5988a33
Jenny Norelius <jenny.norelius@gmail.com>
2015-04-29 18:41:45 +0200

    Corrected search for timestamps in seconds

--- backend/src/com/searcheveryaspect/backend/database/read/QuerierUtil.java
+++ backend/src/com/searcheveryaspect/backend/database/read/QuerierUtil.java
@@ -25,6 +25,8 @@ import java.util.List;
  * 
  */
 class QuerierUtil {
+  private static final int MILLIS_TO_SEC = 1000;
+
   /**
    * Returns a SearchResponse for the specified category, interval and labels containing
    * all parties.
@@ -80,8 +82,9 @@ class QuerierUtil {
             .setQuery(QueryBuilders.matchQuery("party", party.toString()))
             .setQuery(QueryBuilders.matchQuery("category", category.toString()))
             .setPostFilter(
-                FilterBuilders.rangeFilter("publishedTimestamp").from(interval.getStartMillis())
-                    .to(interval.getEndMillis())).execute().actionGet();
+                FilterBuilders.rangeFilter("publishedTimestamp")
+                .from(interval.getStartMillis() / MILLIS_TO_SEC)
+                .to(interval.getEndMillis() / MILLIS_TO_SEC)).execute().actionGet();).execute().actionGet();
 
     // Get the hits as a response
     Iterator<SearchHit> iterator = response.getHits().iterator();
@@ -94,5 +97,4 @@ class QuerierUtil {
 
     return new Entry(list.size(), ImmutableList.copyOf(list));
   }
-
 }
