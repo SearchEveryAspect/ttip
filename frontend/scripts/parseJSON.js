72c4f7f6473b080fbf8e8f21547132748eb3b0cf
aguler <alan.guler@hotmail.com>
2015-05-04 04:40:58 +0200

    added backend request

--- frontend/scripts/parseJSON.js
+++ frontend/scripts/parseJSON.js
@@ -1,38 +1,32 @@
-/**
-@author Alan Guler
-@date 2015-04-07
-*/
+//@author aguler
 
-/*This script will call XMLRequest when called and parse JSON received
-/*Make this also do request?*/
 
-$(document).ready(function() {
+//create xmlhttpreq object
+function makeCorsReq(c, u) {
+  $.ajax({
+    type: 'GET',
+    dataType: 'json',
+    url: u,
+    async: true,
+    success: function(resp){
+      console.log("Response from " + URL + " OK!");
+      if (isJSON(JSON.stringify(resp))) {
+        c.initObj(resp);
+      } else {
+        console.error("Not JSON-data");
+      }
+    },
+    error: function(xhr){
+      console.error("Error status: " + xhr.status);
+    }
+  });
+}
 
-	//com.sea.ttip/prop/cat/month/from/2014-01-01/to/2014-12-31/?party=s,m
-
-    var url= "data.json";
-    var xhr = new XMLHttpRequest();
-    // false means exercise will wait until it gets a response
-    // from server
-    xhr.open("GET", url, false);
-    xhr.send();
-    xhr.onreadystatechange = function() {
-   		if (xhr.readyState == 4 && xhr.status == 200) {
-	   		var jso = JSON.parse(xhr.responseText);
-	   	} else {
-
-	   	}
-	}
-});
-
-//test validity of json
-//TODO also test that the JSON data complies with the BE-FE schema
 function isJSON(str) {
 	try {
 		JSON.parse(str);
 		return true;
 	} catch (e) {
-		console.log("Not JSON-data");
 		return false;
 	}
 }
