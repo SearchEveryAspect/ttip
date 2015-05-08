b9359a8fdff1d625ab8a01e8b9aa06b6ccdc21f2
Mitra Strandberg <mitrastrandberg@gmail.com>
2015-05-08 12:57:49 +0200

    Added code to parse gov response with one hit (now in comments), but it seems to be something wrong with the search request to data.riksdagen...

--- backend/src/com/searcheveryaspect/backend/database/update/URLConnectionReader.java
+++ backend/src/com/searcheveryaspect/backend/database/update/URLConnectionReader.java
@@ -1,6 +1,7 @@
 package com.searcheveryaspect.backend.database.update;
 
 import java.net.*;
+import java.util.ArrayList;
 import java.io.*;
 
 public class URLConnectionReader {
@@ -11,14 +12,51 @@ public class URLConnectionReader {
                                 new InputStreamReader(
                                     connection.getInputStream()));
 
+        
         StringBuilder response = new StringBuilder();
         String inputLine;
 
-        while ((inputLine = in.readLine()) != null) 
+        while ((inputLine = in.readLine()) != null)
             response.append(inputLine);
 
         in.close();
-
+        
+        /*
+        // Ska lägga in [] om det bara är 1 träff
+        String docIsList = "\"dokument\": [";
+        String responseString = response.toString();
+        System.out.println(responseString);
+        if(!responseString.contains(docIsList)) {
+        	//First split
+        	String[] parts = responseString.split("\"dokument\": {");
+        	String part1 = parts[0]; // Before the document split
+        	String part2 = parts[1]; // After the document split
+        	StringBuilder sb = new StringBuilder();
+        	sb.append(part1);
+        	sb.append("\"dokument\": [ {");
+        	sb.append(part2);
+        	String doc = sb.toString();
+        	
+        	//Save the document (doc) in an arraylist (chars)
+        	ArrayList<Character> chars = new ArrayList<Character>();
+        	for(char ch : doc.toCharArray()) {
+        		chars.add(ch);
+        	}
+        	//Remove five last elements in chars
+        	for(int i=0; i<5; i++) {
+        		chars.remove(chars.size()-1);
+        	}
+        	//Add new ending to chars
+        	String newEndString = "      }    ]  }}";
+        	ArrayList<Character> newEndChar = new ArrayList<Character>();
+        	for(char ch : newEndString.toCharArray()) {
+        		newEndChar.add(ch);
+        	}
+        	chars.addAll(newEndChar);
+        	return chars.toString();
+        }
+        */
+        
         return response.toString();
     }
 }
