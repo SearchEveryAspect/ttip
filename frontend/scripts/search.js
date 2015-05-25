b16004734005d3a0b91bb3b4f76491e4dae11fd6
aguler <alan.guler@hotmail.com>
2015-05-25 17:49:04 +0200

    Time fields now autocomplete when month or year have been selected

--- frontend/scripts/search.js
+++ frontend/scripts/search.js
@@ -1,7 +1,3 @@
-/*This script is used on search.html and will wait for user input
-/*before calling parseJSON.js*/
-
-
 
 function SubjectField() {
   this.btn = false;
@@ -41,22 +37,18 @@ SubjectField.prototype = {
   }
 }
 
-function TimePeriod() {
+function TimePeriod(max) {
   this.errorDiv = "#searchbox #error p";
   this.fromId = document.getElementById("from");
   this.toId = document.getElementById("to");
   this.btn = false;
-
-  //Should be gotten from json object.
-  this.maxxi = "2015-05-10" 
-  this.maxDate = Date.parse(this.maxxi);
-
+  this.max = max;
   this.validDateRegex = /^(19[7-9]\d|200\d|201[0-5])[\-](0[1-9]|1[0-2])[\-](0[1-9]|[12]\d|3[01])$/i;
   this.patt = new RegExp(this.validDateRegex);
   this.from;
   this.to;
 }
-//fixa så att search byte rår etc på select inte att man måste stänga kalender-rutan
+
 TimePeriod.prototype = {
   init: function() {
     var c = this;
@@ -66,13 +58,19 @@ TimePeriod.prototype = {
         changeMonth: true,
         changeYear: true,
         numberOfMonths: 1,
-        maxDate: "-5d",
+        maxDate: c.max,
+        onChangeMonthYear: function(y, m, o) {
+          if (m < 10) {
+            m = "0"+m;
+          }
+          $(this).val(y+"-"+m+"-01");
+          c.from = $(this).val();
+        },
         onClose: function( selectedDate ) {
           $( "#to" ).datepicker( "option", "minDate", selectedDate );
           c.to = $( "#to" ).val();
           c.from = $(this).val();
           c.inputHandler();
-
         }
       });
     $( "#to" ).attr("placeholder", "yyyy-mm-dd").datepicker({
@@ -81,7 +79,14 @@ TimePeriod.prototype = {
         changeMonth: true,
         changeYear: true,
         numberOfMonths: 1,
-        maxDate: "-1d",
+        maxDate: c.max,
+        onChangeMonthYear: function(y, m, o) {
+          if (m < 10) {
+            m = "0"+m;
+          }
+          $(this).val(y+"-"+m+"-01");
+          c.to = $(this).val();
+        },
         onClose: function( selectedDate ) {
           $( "#from" ).datepicker( "option", "maxDate", selectedDate );
           c.from = $("#from" ).val();
@@ -91,93 +96,85 @@ TimePeriod.prototype = {
     });
  
   },
+  inputHandler: function() {
 
-    inputHandler: function() {
-      //console.log("Current from is: " + this.from);
-      //console.log("Current to is: " + this.to);
+    if ((this.notDefined(this.from)) || (this.notDefined(this.to))) {
+      this.btn = false;
+      this.clearAll();
+      return;
+    }
 
-      if ((this.notDefined(this.from)) || (this.notDefined(this.to))) {
+    if(this.isValidString(this.from) && 
+        this.isValidString(this.to)) 
+    {    
+      switch(this.getMax(this.from, this.to, this.max)) {
+        case this.from:
+          $("#from").val(this.max);
+          $("#to").val(this.max);
+          this.from = $("#from").val();
+          this.to = $("#to").val();
+
+          break;
+        case this.to:
+          $("#to").val(this.max);
+          this.to = $("#to").val();
+
+          break;
+        case this.max:
+          break;
+      }
+      this.btn = true;
+      this.clearAll();
+    } else {
+        this.fromId.style.borderBottom="2px solid red";
+        this.toId.style.borderBottom="2px solid red";
         this.btn = false;
-        this.clearAll();
+        $(this.errorDiv).text("Inkorrekt tidsformat.");
+    }
+    checkBtn();
+  },
+  isValidString: function(str) {
+    if (this.patt.test(str)) return true;
+    console.error("Not valid string");
+    return false;
+  },
+  getMax: function(_from, _to, _max) {
+    var from = Date.parse(_from);
+    var to = Date.parse(_to);
+    var max = Date.parse(_max);
+    switch(Math.max(from,to,max)) {
+      case max: 
+        return _max;
+        break;
+      case to:
+        return _to;
+        break;
+      case from:
+        return _from;
+        break;
+      default:
+        console.error("Can't getMax of " + _from + ", " + _to + ", " + _max)
         return;
       }
 
-      //Start by checking if its a valid string
-      if(this.isValidString(this.from) && 
-          this.isValidString(this.to)) 
-      {    
-        switch(this.getMax(this.from, this.to, this.maxxi)) {
-          case this.from:
-            $("#from").val(this.maxxi);
-            $("#to").val(this.maxxi);
-            this.from = $("#from").val();
-            this.to = $("#to").val();
+  },
+  notDefined: function(v) {
+    if (v === undefined || v === "") return true;
+    return false;
+  },
+  clearAll: function() {
+    $(this.errorDiv).text("");
+    this.fromId.style.borderBottom = "1px solid rgba(60,60,60, 1)";
+    this.toId.style.borderBottom = "1px solid rgba(60,60,60, 1)";
 
-            break;
-          case this.to:
-            $("#to").val(this.maxxi);
-            this.to = $("#to").val();
-
-            break;
-          case this.maxxi:
-            break;
-        }
-        this.btn = true;
-        this.clearAll();
-      } else {
-          this.fromId.style.borderBottom="2px solid red";
-          this.toId.style.borderBottom="2px solid red";
-          this.btn = false;
-          $(this.errorDiv).text("Inkorrekt tidsformat.");
-      }
-      checkBtn();
-    }
-   
-
-
-    ,
-    isValidString: function(str) {
-      if (this.patt.test(str)) return true;
-      console.error("Not valid string");
-      return false;
-    },
-    getMax: function(_from, _to, _max) {
-      var from = Date.parse(_from);
-      var to = Date.parse(_to);
-      var max = Date.parse(_max);
-      switch(Math.max(from,to,max)) {
-        case max: 
-          return _max;
-          break;
-        case to:
-          return _to;
-          break;
-        case from:
-          return _from;
-          break;
-        default:
-          console.error("Can't getMax of " + _from + ", " + _to + ", " + _max)
-          return;
-        }
-
-    },
-    notDefined: function(v) {
-      if (v === undefined || v === "") return true;
-      return false;
-    },
-    clearAll: function() {
-      $(this.errorDiv).text("");
-      this.fromId.style.borderBottom = "1px solid rgba(60,60,60, 1)";
-      this.toId.style.borderBottom = "1px solid rgba(60,60,60, 1)";
-
-    },
-    getBtn: function() {
-      return this.btn;
-    }
+  },
+  getBtn: function() {
+    return this.btn;
+  }
 }
 
 function searchInit() {
-  tp = new TimePeriod();
+  tp = new TimePeriod("2015-05-20");
   sf = new SubjectField();
   tp.init();
   sf.init();
